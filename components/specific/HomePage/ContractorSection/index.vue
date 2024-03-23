d6e53df9538640aa558c6d6b5e5396fb5d037e59
farnaz <ffarzipour92@gmail.com>
2024-03-23 20:38:53 +0330

    add banner main page

--- components/specific/HomePage/ContractorSection/index.vue
+++ components/specific/HomePage/ContractorSection/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import man2 from '@/assets/images/man/man-2.png'
+import mainBg from '@/assets/images/main-bg.png'
 </script>
 
 <template>
@@ -17,4 +18,36 @@ import man2 from '@/assets/images/man/man-2.png'
       </button>
     </div>
   </div>
+  <div class="w-full relative">
+    <img :src="mainBg" alt="" class="w-full" />
+    <div class="absolute top-0 right-0 w-full grid grid-cols-2 gap-4 h-full">
+      <div></div>
+      <div class="flex flex-col justify-center items-center h-full">
+        <p class="font-[dana-bold] text-primary text-[36px]">
+          قدرت در دستان شماست
+        </p>
+        <p class="font-[dana-medium] text-sec-gray text-[24px] mt-4">
+          با زازا خودتو مجهز کن
+        </p>
+        <p class="text-white text-sm mt-7 cursor-pointer has-line">
+          مشاهده محصولات
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss">
+.has-line {
+  position: relative;
+  &::before {
+    content: '';
+    position: absolute;
+    bottom: -5px;
+    right: 0;
+    width: 30px;
+    height: 2px;
+    background: #fff;
+  }
+}
+</style>
