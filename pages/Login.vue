c32a019e79cc5d710f9251874406a760e7bb3197
farnaz <ffarzipour92@gmail.com>
2024-03-25 21:41:09 +0330

    login and otp page

---
+++ pages/Login.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Logo from '@/assets/images/logo.svg'
+
+definePageMeta({
+  layout: 'none!',
+})
+</script>
+
+<template>
+  <div class="w-full h-screen flex justify-center items-center">
+    <div class="rounded border  lg:border-sec-gray p-10 text-center">
+      <img :src="Logo" alt="zaza kala" class="mb-4 w-40 mx-auto" />
+      <p class="font-[dana-extra] font-xl">ورود | ثبت‌نام</p>
+      <p class="font-[dana-light] mt-3">سلام!</p>
+      <p class="font-[dana-light] mt-3">
+        لطفا شماره موبایل یا ایمیل خود را وارد کنید
+      </p>
+      <input
+        type="number"
+        class="w-[300px] lg:w-[400px] border-2 border-primary focus:outline-none py-3 mt-4 rounded indent-2"
+      />
+      <button
+        class="w-[300px] lg:w-[400px] block p-4 bg-primary rounded focus:outline-none text-white mt-10"
+      >
+        ارسال کد تایید
+      </button>
+    </div>
+  </div>
+</template>
---
+++ pages/otp.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Logo from '@/assets/images/logo.svg'
+
+definePageMeta({
+  layout: 'none!',
+})
+</script>
+
+<template>
+  <div class="w-full h-screen flex justify-center items-center">
+    <div
+      class="rounded  border lg:border-sec-gray p-10 text-center"
+    >
+      <img :src="Logo" alt="zaza kala" class="mb-4 w-40 mx-auto" />
+      <p class="font-[dana-extra] font-xl">کد تایید را وارد کنید</p>
+      <p class="font-[dana-light] mt-3">
+        کد تایید برای شماره ۰۹۲۱۲۰۷۹۳۶۹ پیامک شد
+      </p>
+
+      <input
+        type="number"
+        class="w-[300px] lg:w-[400px] border-2 border-primary focus:outline-none py-3 mt-4 rounded indent-2"
+      />
+      <p class="mt-10 font-[dana-light] font-sm">
+        ۱۸ : ۰۲مانده تا دریافت مجدد کد
+      </p>
+      <button
+        class="w-[300px] lg:w-[400px] block p-4 bg-primary rounded focus:outline-none text-white mt-2"
+      >
+        ورود
+      </button>
+    </div>
+  </div>
+</template>
