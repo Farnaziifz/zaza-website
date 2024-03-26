1aab10d93f689a6011ece1fc27b2873131b14ddc
Farnaz <farzipour.f@gmail.com>
2024-03-26 14:41:54 +0330

    service page

---
+++ components/specific/ServicesPage/ServicesCategory/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container my-14">
+    <Swiper
+      :modules="[SwiperAutoplay, SwiperPagination]"
+      :slides-per-view="1"
+      :loop="true"
+      :pagination="{
+        el: '.custom-pagination',
+        enabled: true,
+      }"
+      :autoplay="{
+        delay: 5000,
+        disableOnInteraction: true,
+      }"
+      class="w-full"
+    >
+      <SwiperSlide v-for="slide in 10" :key="slide">
+        <strong>عکس بنر {{ slide }}</strong>
+      </SwiperSlide>
+    </Swiper>
+    <div class="text-center">
+      <div class="custom-pagination !w-auto mt-5" self-center></div>
+    </div>
+  </div>
+</template>
---
+++ pages/services.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import ServicesCategory from '@/components/specific/ServicesPage/ServicesCategory/index.vue'
+</script>
+
+<template>
+  <div class="main-page">
+    <div class="page-shadow">
+      <div class="mt-14 bg-sec-gray h-full">
+        <ServicesCategory />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.main-page {
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
+  .page-shadow {
+    height: 500px;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: linear-gradient(
+      180deg,
+      rgba(2, 0, 36, 1) 0%,
+      rgba(252, 186, 46, 0.738813025210084) 0%,
+      rgba(255, 255, 255, 1) 82%
+    );
+  }
+}
+</style>
